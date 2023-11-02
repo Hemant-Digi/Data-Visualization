2cd04566e16826f48f0bd8333f7045b0d6203128
Hemant-Digi <hemant.bisht@digimantra.com>
2023-11-02 12:26:09 +0530

    data visualization task

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import './App.css';
+import wineData from './utils/Wine-Data.json';
+import { calculateMean, calculateMedian, calculateMode } from './utils/statistics';
+import { WineGammaStatisticsComponent } from './components/Gamma';
+
+// Separate data by classes
+const classes = {};
+wineData.forEach(item => {
+  const alcoholClass = item.Alcohol;
+  if (!classes[alcoholClass]) {
+    classes[alcoholClass] = [];
+  }
+  classes[alcoholClass].push(item.Flavanoids);
+});
+
+
+// Calculate mean, median, and mode for each class
+const result = {};
+for (const classKey in classes) {
+  const FlavanoidsValues = classes[classKey];
+  result[classKey] = {
+    Mean: calculateMean(FlavanoidsValues),
+    Median: calculateMedian(FlavanoidsValues),
+    Mode: calculateMode(FlavanoidsValues),
+  };
+}
+
+// Display the results in React component format
+function WineStatisticsComponent() {
+  return (
+    <><>
+    </><table>
+        <thead>
+          <tr>
+            <th>Measure</th>
+            {Object.keys(result).map(classKey => (
+              <th key={classKey}>Class {classKey}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Flavanoids (Mean, Median, Mode)</td>
+            {Object.keys(result).map(classKey => (
+              <td key={classKey}>
+                {result[classKey]?.Mean?.toFixed(2)} <br />
+                {result[classKey]?.Median?.toFixed(2)} <br />
+                {result[classKey]?.Mode}
+              </td>
+            ))}
+          </tr>
+        </tbody>
+      </table><h1>Wine Gamma Statistics</h1><WineGammaStatisticsComponent /></>
+  );
+}
+
+export default WineStatisticsComponent;
---
+++ src/components/Gamma.js
@@ -0,0 +1,52 @@
+import wineData from '../utils/Wine-Data.json';
+import { calculateStatistics } from '../utils/statistics';
+
+  // Calculate Gamma property for each data point
+  wineData.forEach(item => {
+    item.Gamma = (item.Ash * item.Hue) / item.Magnesium;
+  });
+  
+  // Separate data by classes for the new property "Gamma"
+  const gammaClasses = {};
+  wineData.forEach(item => {
+    const alcoholClass = item.Alcohol;
+    if (!gammaClasses[alcoholClass]) {
+      gammaClasses[alcoholClass] = [];
+    }
+    gammaClasses[alcoholClass].push(item.Gamma);
+  });
+
+  // Calculate mean, median, and mode for each class for the new property "Gamma"
+  const gammaStatistics = {};
+  for (const classKey in gammaClasses) {
+    gammaStatistics[classKey] = calculateStatistics(gammaClasses[classKey]);
+  }
+  
+  // Display the results in React component format
+  export function WineGammaStatisticsComponent() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Measure</th>
+            {Object.keys(gammaStatistics).map(classKey => (
+              <th key={classKey}>Class {classKey}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Gamma (Mean, Median, Mode)</td>
+            {Object.keys(gammaStatistics).map(classKey => (
+              <td key={classKey}>
+                {gammaStatistics[classKey]?.Mean} <br />
+                {gammaStatistics[classKey]?.Median} <br />
+                {gammaStatistics[classKey]?.Mode}
+              </td>
+            ))}
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+  
